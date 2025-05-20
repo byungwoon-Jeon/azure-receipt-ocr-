e798c6173af66c4c519501c318dac8a6358a4728
byungwoon Jeon <quddnsrnt@naver.com>
2025-05-20 14:52:17 +0900

    drm_test.py 업데이트

--- tracing/drm_test.py
+++ tracing/drm_test.py
@@ -11,3 +11,18 @@ try:
         print("PDF는 열 수 있습니다.")
 except Exception as e:
     print("PDF 열기 실패:", e)
+    
+import zipfile
+
+docx_path = "./your_file.docx"
+
+try:
+    with zipfile.ZipFile(docx_path, 'r') as z:
+        z.testzip()
+    print("DOCX 접근 가능 (DRM 없음)")
+except Exception as e:
+    print("DOCX 접근 실패 (DRM 가능성 있음):", e)
+
+
+
+
